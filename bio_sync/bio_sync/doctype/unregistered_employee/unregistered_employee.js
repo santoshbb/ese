6146f0bea883ec12b0aa06acf8ecba0c98ba73ec
Sammish Thundiyil <sammish.thundiyil@gmail.com>
2019-08-16 18:42:26 +0300

    new file:   bio_sync/bio_sync/doctype/__init__.py 	new file:   bio_sync/bio_sync/doctype/__pycache__/__init__.cpython-36.pyc 	new file:   bio_sync/bio_sync/doctype/unregistered_employee/__init__.py 	new file:   bio_sync/bio_sync/doctype/unregistered_employee/__pycache__/__init__.cpython-36.pyc 	new file:   bio_sync/bio_sync/doctype/unregistered_employee/__pycache__/unregistered_employee.cpython-36.pyc 	new file:   bio_sync/bio_sync/doctype/unregistered_employee/test_unregistered_employee.py 	new file:   bio_sync/bio_sync/doctype/unregistered_employee/unregistered_employee.js 	new file:   bio_sync/bio_sync/doctype/unregistered_employee/unregistered_employee.json 	new file:   bio_sync/bio_sync/doctype/unregistered_employee/unregistered_employee.py

---
+++ bio_sync/bio_sync/doctype/unregistered_employee/unregistered_employee.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Sammish and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Unregistered Employee', {
+	// refresh: function(frm) {
+
+	// }
+});
